19bf2c1ad6be17f875fb058489e73d8d2a1d2819
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-06-17 22:47:00 +0900

    java-1-6[start]

---
+++ src/Carc.java
@@ -0,0 +1,9 @@
+
+public class Carc {
+	public static void main(String[] args) {
+		int a = 10; int b = 2;
+		int total = CarcLogic.tasu(a,b);
+		int delta = CarcLogic.hiku(a,b);
+		System.out.println(a+"と"+b+"足すと"+total+"引くと"+delta);
+	}
+}
---
+++ src/CarcLogic.java
@@ -0,0 +1,10 @@
+
+public class CarcLogic {
+	public static int tasu(int a, int b) {
+		return(a+b);
+	}
+
+	public static int hiku(int a, int b) {
+		return(a-b);
+	}
+}

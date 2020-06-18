1ab145574df3b581a32d5dff1181694d81b9b463
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-06-18 12:56:38 +0900

    java-1-6-3[finish]

--- src/calcapp/logics/CalcLogic.java
+++ src/calcapp/logics/CalcLogic.java
@@ -1,4 +1,4 @@
-
+package calcapp.logics;
 public class CalcLogic {
 	public static int tasu(int a, int b) {
 		return(a+b);
--- src/calcapp/main/Calc.java
+++ src/calcapp/main/Calc.java
@@ -1,7 +1,8 @@
-
+package calcapp.main;
+import calcapp.logics.*;
 public class Calc {
 	public static void main(String[] args) {
-		int a = 10; int b = 2;
+		int a = 100; int b = 2;
 		int total = CalcLogic.tasu(a,b);
 		int delta = CalcLogic.hiku(a,b);
 		System.out.println(a+"と"+b+"足すと"+total+"引くと"+delta);

b68af4867a6ba01a71bb89844fedfdb522911442
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-06-18 14:09:25 +0900

    java1-6[finish]

---
+++ question/Main.java
@@ -0,0 +1,9 @@
+import comment.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		Kouhan.callDean();
+		Kouhan.showMondokoro();
+	}
+}
---
+++ question/comment/Kouhan.java
@@ -0,0 +1,13 @@
+package comment;
+public class Kouhan {
+	public static void callDean() {
+		System.out.println("えぇい、小癪な。曲者だ！出会えい！");
+	}
+
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Thread.sleep(3000);
+		Zenhan.doTogame();
+	}
+}
